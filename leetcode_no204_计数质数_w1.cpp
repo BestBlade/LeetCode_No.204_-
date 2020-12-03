88630a6e131eb9d78d8f78a235e7074c322c536e
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-12-03 14:20:43 +0800

    Add files via upload

---
+++ leetcode_no204_计数质数_w1.cpp
@@ -0,0 +1,74 @@
+﻿/* ------------------------------------------------------------------|
+统计所有小于非负整数 n 的质数的数量。
+
+ 
+
+示例 1：
+
+输入：n = 10
+输出：4
+解释：小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 。
+示例 2：
+
+输入：n = 0
+输出：0
+示例 3：
+
+输入：n = 1
+输出：0
+ 
+
+提示：
+
+0 <= n <= 5 * 106
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/count-primes
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	埃氏筛
+*
+*	执行用时：44 ms, 在所有 C++ 提交中击败了84.28%的用户
+*	内存消耗：19.4 MB, 在所有 C++ 提交中击败了5.03%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+int countPrimes(int n) {
+    vector<int> isPrime(n, 1);
+    int ans = 0;
+    for (int i = 2; i < n; i++) {
+        if (isPrime[i]) {
+            ans++;
+            if ((long long)i * i < n) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = 0;
+                }
+            }
+        }
+    }
+    return ans;
+}
